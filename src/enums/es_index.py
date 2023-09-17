f30277f0309686126056a8b4c90d595986d59d60
likeinlife <likeinlife48@yandex.ru>
2023-09-17 11:52:04 +0300

    fix(es_index.py): rename `movies` -> `movie`

--- src/enums/es_index.py
+++ src/enums/es_index.py
@@ -4,6 +4,6 @@ from aenum import StrEnum
 class EsIndex(StrEnum):
     """Названия индексов в ElasticSearch."""
 
-    MOVIES = 'movies'
+    MOVIE = 'movie'
     GENRE = 'genre'
     PERSON = 'person'
