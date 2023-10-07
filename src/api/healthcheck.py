9260d6f05e6b89a756b210222bfbe3741fa8c807
Maxim Zaitsev <zaitsevmaxim@yandex.ru>
2023-10-07 12:33:03 +0300

    Task 09/add open api docs (#21)
    * refactor class
    
    (cherry picked from commit 89e5f5b26da1c2c0a4fbdecadbdc96adf5553cc2)
    
    * add doc
    
    (cherry picked from commit bdd39a33f4e84f671ed34e05710a932de5948b98)
    
    * add film id doc
    
    (cherry picked from commit 154559e5ebad7d602b72248e5e8e67a38816ccd3)
    
    * add docs for film filter
    
    (cherry picked from commit 86551e6b4b275ba733238394d1e66fee41d22c32)
    
    * add genre doc
    
    (cherry picked from commit 40f931111f904b1ffe8f8f51326a1a35b0a4cf84)
    
    * fix
    
    (cherry picked from commit bad9ef31a444f388d1a8a502927faf46dffcbfb5)
    
    * add person docs
    
    (cherry picked from commit e16f15e1721a6eb46d192a64afcd4d5ae3488186)
    
    * fix
    
    (cherry picked from commit faa6c3a9118a85d91b12a82be9eb0bc31b0fae4f)
    
    * translate
    
    (cherry picked from commit b057723adadc06a974c7f80ce3890fe1bdcd4d09)
    
    * minor(healthcheck.py): remove word
    
    ---------
    
    Co-authored-by: likeinlife <likeinlife48@yandex.ru>
    Co-authored-by: likeinlife <99913000+likeinlife@users.noreply.github.com>

--- src/api/healthcheck.py
+++ src/api/healthcheck.py
@@ -14,7 +14,7 @@ class HealthCheck(BaseModel):
     '/health',
     tags=['healthcheck'],
     summary='Проверка состояния сервиса',
-    response_description='Возвращает состояние сервиса',
+    response_description='Состояние сервиса',
     status_code=status.HTTP_200_OK,
     response_model=HealthCheck,
 )
@@ -23,6 +23,6 @@ def get_health() -> HealthCheck:
     Проверка состояния сервиса.
 
     Returns:
-        HealthCheck: Возвращает состояние сервиса
+        HealthCheck: Состояние сервиса
     """
     return HealthCheck(status='OK')
